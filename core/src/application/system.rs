93feb15dd13c0a8c05f27d6d7bc8fa7b0f5aecab
Natty <31473117+AMNatty@users.noreply.github.com>
2022-10-19 02:09:11 +0200

    System docs

--- core/src/application/system.rs
+++ core/src/application/system.rs
@@ -24,6 +24,9 @@
 
 use std::any::Any;
 
+/// A utility trait for systems to support dynamic typing.
+///
+/// Automatically implemented for all systems.
 pub trait SystemDyn: 'static {
     fn as_any(&self) -> &dyn Any;
     fn as_any_mut(&mut self) -> &mut dyn Any;
@@ -47,4 +50,5 @@ where
     }
 }
 
+/// The base trait for all systems.
 pub trait System: SystemDyn {}
